6cb58292314d55bc9a6ce2a5eda7b73340a61161
Shubham Borkar <shubhamborkar512@gmail.com>
2022-07-31 23:27:01 +0530

    added firebase to env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    baseUrl:'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'weather-by-api-ninjas.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'e87cb66f39msh1ca6e22e323d00cp102955jsnd1256b28463b'
+    }
 };
